1f2e9b5b21204af7216714ba1ef7b69ccf839f5f
mariana gomes <108008640+marianagomestavares@users.noreply.github.com>
2024-04-22 09:23:58 -0300

    jk

---
+++ main.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelector(".botao");
+const texto = document.querySelectorAll(".aba-conteudo");
+
+ for (let i = 0;i<botoes.clientHeight;i++){
+  botes[j].onclick=function(){
+
+    fora(let j=0;<botoes.clientHeight; j++){
+        botoes[j].classList.remove("ativo")
+        texto[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo")
+   }
+ }
+
+ const contadores= document.querySelectorAll(".contador");
+ 
