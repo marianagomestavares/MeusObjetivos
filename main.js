853a353b65430e357b9b052777117799917043b5
mariana gomes <108008640+marianagomestavares@users.noreply.github.com>
2024-04-25 10:54:03 -0300

    jk

--- main.js
+++ main.js
@@ -14,4 +14,26 @@ const texto = document.querySelectorAll(".aba-conteudo");
  }
 
  const contadores= document.querySelectorAll(".contador");
- 
+ const tempoObjeti1vo= new Date ("2023-10-05T00:00:00");
+ const tempoObjetivo2=new Date("2023-12-05T00:00:00") ;
+ const tempoObjetivo3=new Date("2023-12-30T00:00:oo");
+ const tempoObjetivo4=nem Date("2024-02-01T00:00:00");
+ const tempos=
+ [tempoObjetivo1,tempoObjrtivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+ function calculaTempo(tempoObjetivo){
+  let tempoAtual=nem Date();
+  let tempoFinal=tempoObjetivo-tempoAtual;
+  let segundos=Math.floor(tempoAtual/1000);
+  let minutos=math.floor(segundos/60);
+  let dias= Math.floor(horas/24);
+
+  segundos%=60;
+  minutos%=60;
+  horas%=24
+  if(tempo final>0){return[dias,horas,minutos;segundos];
+  }else{
+    return[0,0,0,0];
+    
+ }
